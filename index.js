16f91eb18c7fb7ba3a0bf693d814e4a40dcc9768
jackmacaa <jackmaca@msn.com>
2021-11-05 22:00:30 +1030

    Added option for process environment port

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -128,6 +130,6 @@ app.get('/api/3', (req, res) => {
     res.send(`${diff}`);
 });
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
+app.listen(PORT, () => {
+    console.log('Serving on port ' + `${PORT}`)
+});
